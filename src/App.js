7818a2706bba26b3152cbed2d194c9fbbefd7090
Sayan <sayanroy2802@gmail.com>
2024-10-05 01:47:12 +0530

    app js changed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Menu from './components/Menu';
 import Cart from './components/Cart';
 import Home from './components/Home';
 import Footer from './components/Footer';
-import NotFound from './components/NotFound'
+// import NotFound from './components/NotFound'
 
 
 function App() {
@@ -12,10 +12,10 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="*" element={<Home/>} />
+        <Route path="/" element={<Home/>} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/" element={<NotFound />} />
+        {/* <Route path="/" element={<NotFound />} /> */}
       </Routes>
       <Footer/>
     </Router>
